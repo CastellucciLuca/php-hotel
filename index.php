8ccb4b6e1ed314de6635b89ac877e1d54fa840fd
CastellucciLuca <castellucciluca98@gmail.com>
2023-01-25 15:29:20 +0100

    hotel parking foreach and add table with bootstrap

--- index.php
+++ index.php
@@ -39,40 +39,57 @@ $hotels = [
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Hotel</title>
-    <link rel="stylesheet" href="./styles/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 <body>
     <main>
-        <ol>
-            <?php
-            foreach ($hotels as $key => $hotel) {
-                echo "
-            <li>
-                <h2>
-                    {$hotel['name']}
-                </h2>
-                <p>
-                    {$hotel['description']}
-                </p>
-                <span>
-                Parcheggio:
-                    {$hotel['parking']}
-                </span>
-                <p>
-                Distanza dal centro:
-                    {$hotel['distance_to_center']}
-                </p>
-            </li>
+        <table class="table table-danger table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $key => $hotel) {
+                    
+                    if ($hotel['parking']) {
+                        $parking = 'Si';
+                    } else {
+                        $parking = 'No';
+                    }
+                    echo "
+                <tr>
+                <th>{$key}</th>
+                    <td class='fw-bold'>
+                        {$hotel['name']}
+                    </td>
+                    <td>
+                        {$hotel['description']}
+                    </td>
+                    <td>
+                        {$parking}
+                    </td>
+                    <td>
+                        {$hotel['distance_to_center']}
+                    </td>
+                </tr>
             ";
-            };
-            ?>
-        </ol>
+                };
+                ?>
+            </tbody>
+        </table>
     </main>
 </body>
+
 </html>
